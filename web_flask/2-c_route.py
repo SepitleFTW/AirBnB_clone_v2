fd40313d03262affc6e65113c87ea21acd4a0050
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-04-21 14:53:50 +0200

    web flask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,31 +1,38 @@
 #!/usr/bin/python3
 """Initializes a Flask web application.
-
 The application listens on 0.0.0.0, port 5000.
 Routes:
     /: Displays 'Hello HBNB!'.
     /hbnb: Displays 'HBNB'.
     /c/<text>: Displays 'C' followed by the value of <text>.
 """
+
 from flask import Flask
 
 app = Flask(__name)
 
 
 app.route("/", strict_slashes=False)
+
+
 def hello_hbnb():
     return "Hello HBNB!"
 
 
 app.route("/hbnb", strict_slashes=False)
+
+
 def display_hbnb():
     return "HBNB"
 
 
 app.route("/c/<text>", strict_slashes=False)
+
+
 def c_text():
     text = text.replace("_", " ")
     return "C {}".format(text)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
