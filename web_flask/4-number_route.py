d5d6e5c0bdc6edaf8772cf724288540c252b1b75
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-04-21 15:37:09 +0200

    web flask

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -10,49 +10,36 @@ Routes:
     /number/<n>: Illustrates 'n is a number' solely if n be an integer.
 """
 from flask import Flask
-from flask import abort
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Showcases 'Greetings, Esteemed HBNB!'."""
-    return "Greetings, Esteemed HBNB!"
+def hello():
+    return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Exemplifies 'HBNB'."""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
-    """Portrays 'C' followed by the value of <text>.
-
-    Replaces any underscores in <text> with slashes.
-    """
-    text = text.replace("_", " ")
-    return "C {}".format(text)
+    return "C " + text.replace("_", " ")
 
 
-@app.route("/python", strict_slashes=False)
+@app.route("/python/", default={"text": "is cool"}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
-def python(text="is cool"):
-    """Exhibits 'Python' followed by the value of <text>.
-
-    Replaces any underscores in <text> with slashes.
-    """
-    text = text.replace("_", " ")
-    return "Python {}".format(text)
+def python(text):
+    return "Python " + tetx.replace("_", " ")
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
+@app.route("/number/n", strict_slashes=False)
 def number(n):
-    """Illustrates 'n is a number' solely if n be an integer."""
-    return "{} is a number".format(n)
+    if isinstance(n, int):
+        return "n is a number"
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if__name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
