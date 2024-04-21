84742cf3d2d125a20b28ffc4fbfc8be724bd7665
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-04-21 15:05:44 +0200

    fixed decorater issue @

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -13,22 +13,16 @@ app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-
-
 def hello_hbnb():
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
-
-
 def display_hbnb():
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
-
-
 def c(text):
     return "C " + text.replace("_", " ")
 
