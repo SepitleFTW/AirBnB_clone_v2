abefbd307b83741ca6a6cfd35b2042a673fbb27d
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-04-22 20:16:29 +0200

    ...

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-"""Initiates a Flask web application.
-
-The application is set to listen on 0.0.0.0, port 5000.
-Routes:
-    /states_list: An HTML page containing a list of all State objects in DBStorage.
+"""
+start Flask application
 """
 
 from flask import Flask, render_template
@@ -12,28 +9,18 @@ from models import storage
 app = Flask(__name__)
 
 
-
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-
-    States are arranged alphabetically.
-    """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
-
-
-@app.route("/states_list", strict_slashes=False)
-def states_list():
+    """display a HTML page with the states listed in alphabetical order"""
     states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template("7-states_list.html", states=states)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Closes the current SQLAlchemy session."""
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
