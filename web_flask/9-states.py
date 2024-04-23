a852255c81349664865d4a87aba1b064dbc2dff5
SepitleFTW <sepitleleshilo69@gmail.com>
2024-04-23 10:37:53 +0000

    ...

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -25,7 +25,8 @@ def states():
 
 @app.route("/states/<id>", strict_slashes=False)
 def states_id(id):
-    """Displays an HTML page with information about the specified <id> state, if it exists."""
+    """Displays an HTML page with information about the
+ specified <id> state, if it exists."""
     for state in storage.all("State").values():
         if state.id == id:
             return render_template("9-states.html", state=state)
